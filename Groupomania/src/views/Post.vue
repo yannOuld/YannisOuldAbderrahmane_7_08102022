1539a001d288d2ff6c1b618d6778a35f59779fb2
Yannis ould abderrahmane <yannis.ouldab@outlook.fr>
2023-02-16 05:26:19 +0100

    Post view setup script

--- Groupomania/src/views/Post.vue
+++ Groupomania/src/views/Post.vue
@@ -1,68 +1,39 @@
-<script>
+<script setup>
+  import PostCard from "../components/Post/PostCard.vue";
+  import LikeModal from "../components/Like/LikeModal.vue";
+  import PostUtils from "../components/Post/PostUtils.vue";
+  import Comments from "../components/Layout/Comments.vue";
   import { useRoute } from "vue-router";
-  import { ref, defineAsyncComponent } from "vue";
+  import { ref } from "vue";
   import { storeToRefs } from "pinia";
   import { usePostStore } from "../stores/posts";
   import { useAuthStore } from "../stores/auth";
 
-  export default {
-    name: "PostView",
-    components: {
-      PostCard: defineAsyncComponent({
-        loader: () => import("../components/Post/PostCard.vue"),
-        delay: 1000,
-      }),
-      LikeModal: defineAsyncComponent({
-        loader: () => import("../components/Like/LikeModal.vue"),
-        delay: 1000,
-      }),
+  const update = ref(0);
+  const forceUpdate = ref(0);
 
-      PostUtils: defineAsyncComponent({
-        loader: () => import("../components/Post/PostUtils.vue"),
-        delay: 1000,
-      }),
-      Comments: defineAsyncComponent({
-        loader: () => import("../components/Layout/Comments.vue"),
-        delay: 1000,
-      }),
-    },
+  // post uuid from the params
+  const route = useRoute();
+  const uuid = route.params.uuid;
 
-    setup() {
-      const update = ref(0);
-      const forceUpdate = ref(0);
+  // fetch and find post data from the store
+  const { fetchOnePost } = usePostStore();
+  fetchOnePost(uuid);
+  const { post } = storeToRefs(usePostStore());
 
-      // post uuid from the params
-      const route = useRoute();
-      const uuid = route.params.uuid;
+  // finding user data
+  const { userData } = useAuthStore();
+  const { user } = userData;
 
-      // fetch and find post data from the store
-      const { fetchOnePost } = usePostStore();
-      fetchOnePost(uuid);
-      const { post } = storeToRefs(usePostStore());
+  const updateComments = () => {
+    forceUpdate.value += 1;
+  };
 
-      // finding user data
-      const { userData } = useAuthStore();
-      const { user } = userData;
-
-      const updateComments = () => {
-        forceUpdate.value += 1;
-      };
-
-      const updatePost = () => {
-        update.value += 1;
-      };
-      return {
-        user,
-        uuid,
-        post,
-        update,
-        forceUpdate,
-        updatePost,
-        updateComments,
-      };
-    },
+  const updatePost = () => {
+    update.value += 1;
   };
 </script>
+
 <template>
   <div class="post w-full">
     <navigation-links></navigation-links>
