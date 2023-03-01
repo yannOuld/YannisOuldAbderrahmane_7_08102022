5d35201d2f61ce781424793d1f77030d30339e1a
Yannis ould abderrahmane <yannis.ouldab@outlook.fr>
2023-03-01 04:28:31 +0100

    app height min

--- Groupomania/src/App.vue
+++ Groupomania/src/App.vue
@@ -8,6 +8,6 @@
 
 <style scoped>
   .app {
-    @apply w-full bg-groupomania bg-fixed bg-origin-border bg-no-repeat bg-center mt-44 sm:mt-24;
+    @apply w-full min-h-screen bg-groupomania bg-fixed bg-origin-border bg-no-repeat bg-center mt-44 sm:mt-24;
   }
 </style>
